9508922f5293a4549a7d6eddeb57b7f2d9206d64
rodrigo-lopez <rodrigo.lopez@bicevida.cl>
2014-03-19 09:51:36 -0300

    Creación de ejb de póliza

---
+++ Model/src/cl/bicevida/services/colectivos/model/PolizaEJB.java
@@ -0,0 +1,7 @@
+package cl.bicevida.services.colectivos.model;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface PolizaEJB {
+}
---
+++ Model/src/cl/bicevida/services/colectivos/model/PolizaEJBBean.java
@@ -0,0 +1,15 @@
+package cl.bicevida.services.colectivos.model;
+
+import javax.annotation.Resource;
+
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+
+@Stateless(name = "PolizaEJB", mappedName = "ServiciosColectivos-PolizaEJB")
+public class PolizaEJBBean implements PolizaEJB {
+    @Resource
+    SessionContext sessionContext;
+
+    public PolizaEJBBean() {
+    }
+}
